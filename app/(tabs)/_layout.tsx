57d1794a12ab8b514538ce8485ebb2ae0c48833f
nguyenanninh1212212 <ninh56085@gmail.com>
2025-01-06 15:03:55 +0700

    add

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,75 @@
 import { Tabs } from "expo-router";
-import React from "react";
-import { Platform } from "react-native";
+import React, { useRef, useEffect } from "react";
+import { Animated, Platform, View } from "react-native";
 
 import { HapticTab } from "@/components/HapticTab";
 import { IconSymbol } from "@/components/ui/IconSymbol";
 import TabBarBackground from "@/components/ui/TabBarBackground";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
+
+function AnimatedTabIcon({
+  name,
+  color,
+  focused,
+}: {
+  name: string;
+  color: string;
+  focused: boolean;
+}) {
+  const translateY = useRef(new Animated.Value(0)).current; // Initial position
+
+  useEffect(() => {
+    if (focused) {
+      // Move up
+      Animated.spring(translateY, {
+        toValue: -30,
+        friction: 4,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      // Move back to original position
+      Animated.spring(translateY, {
+        toValue: 0,
+        friction: 4,
+        useNativeDriver: true,
+      }).start();
+    }
+  }, [focused, translateY]);
+
+  return (
+    <Animated.View
+      style={{
+        transform: [{ translateY }],
+        width: 75,
+        height: 75,
+        borderRadius: 100,
+        backgroundColor: "#ffffff",
+        justifyContent: "center",
+        alignItems: "center",
+        position: "absolute",
+      }}
+    >
+      <Animated.View
+        style={{
+          backgroundColor: focused ? "#9fd7f9" : "",
+          borderRadius: 100,
+          width: 60,
+          height: 60,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <IconSymbol
+          size={focused ? 35 : 28}
+          backgroundColor={focused ? "#ffffff" : "transparent"}
+          name={name}
+          color={focused ? "#ffffff" : "#212121"}
+        />
+      </Animated.View>
+    </Animated.View>
+  );
+}
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -15,69 +78,68 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        headerShown: false,
+        headerShown: true,
         tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
-          ios: {
-            position: "fixed",
-            bottom: 50,
-            width: "90%",
-            borderRadius: 20,
-            overflow: "hidden",
-            marginLeft: "auto",
-            marginRight: "auto",
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
-            borderTopWidth: 0,
-            borderTopColor: "transparent",
-          },
-          android: {
-            position: "fixed",
-            bottom: 50,
-            borderRadius: 20,
-            overflow: "hidden",
-            marginLeft: "auto",
-            marginRight: "auto",
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
-            borderTopWidth: 0,
-            borderTopColor: "transparent",
-          },
+          ios: styles,
+          android: styles,
           default: {},
         }),
       }}
     >
       <Tabs.Screen
-        name="index"
+        name="index2"
         options={{
           title: "",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
+          tabBarIcon: ({ color, focused }) => (
+            <AnimatedTabIcon name="hand.wave" color={color} focused={focused} />
           ),
         }}
       />
       <Tabs.Screen
-        name="index2"
+        name="index"
         options={{
           title: "",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="hand.wave" color={color} />
+          tabBarIcon: ({ color, focused }) => (
+            <AnimatedTabIcon
+              name="house.fill"
+              color={color}
+              focused={focused}
+            />
           ),
         }}
       />
-
       <Tabs.Screen
         name="index3"
         options={{
           title: "",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="pin.square.fill" color={color} />
+          tabBarIcon: ({ color, focused }) => (
+            <AnimatedTabIcon
+              name="pin.square.fill"
+              color={color}
+              focused={focused}
+            />
           ),
         }}
       />
     </Tabs>
   );
 }
+
+import { ViewStyle } from "react-native";
+
+const styles: ViewStyle = {
+  position: "absolute",
+  borderRadius: 20,
+  marginLeft: "auto",
+  marginRight: "auto",
+  flexDirection: "row",
+  justifyContent: "center",
+  alignItems: "center",
+  borderTopWidth: 0,
+  borderTopColor: "transparent",
+  shadowColor: "#000",
+  shadowOffset: { width: 0, height: 4 },
+  shadowOpacity: 0.1,
+  shadowRadius: 5,
+};
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,8 +3,8 @@ import { Text, SafeAreaView, View } from "react-native";
 const HomeScreeen = () => {
   return (
     <SafeAreaView>
-      <View className="w-screen h-screen flex justify-center items-center">
-        <Text className="text-red-500">Nguyễn An Ninh</Text>
+      <View className="w-screen h-screen flex justify-center items-center bg-blue-400">
+        <Text className="text-red-500 bg-p">Nguyễn An Ninh</Text>
       </View>
     </SafeAreaView>
   );
